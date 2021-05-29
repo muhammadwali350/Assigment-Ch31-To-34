ff411a6de25ecf3e19a20a4c7fa1ca74588d11a0
mohammadwalikhan <mohammadwalikhan1@github.com>
2021-05-29 21:29:18 +0500

    7 question completed only i am working on more

---
+++ app.js
@@ -0,0 +1,138 @@
+// Task 1 //
+
+// var rightNow = new Date();
+// console.log(rightNow);
+
+// Task 2 //
+
+// var monthName = ["January", "February", "March", "april", "May", "june", "July", "aug", "sep", "oct", "november", "december"]
+// var now  = new Date();
+// var theMonth = now.getMonth();
+// var nameOfMonth = monthName[theMonth]; 
+// console.log(nameOfMonth);
+
+// Task 3 //
+
+// var currentDay = new Date();
+// currentDay.toString;
+// var b = currentDay.toString();
+// var c = b.slice(0,3);
+// var weekday = new Array(7);
+// weekday[0] = "Sunday";
+// weekday[1] = "Monday";
+// weekday[2] = "Tuesday";
+// weekday[3] = "Wednesday";
+// weekday[4] = "Thursday";
+// weekday[5] = "Friday";
+// weekday[6] = "Saturday";
+
+// alert("Today Is" + " "  + c);
+
+
+// Task 4 //
+
+// var currentDate = new Date();
+// currentDate.toString;
+// var b = currentDate.toString();
+// var c = b.slice(0,3);
+// if (c === "Sat"){
+//     document.write("It's Fun Day")
+// }else if(c === "Sun"){
+//     document.write("It's Fun Day")
+// }else if(c === "Mon"){
+//     document.write("Work Day")
+// }else if(c === "Tue"){
+//     document.write("Work Day")
+// }else if(c === "Wed"){
+//     document.write("Work Day")
+// }else if(c === "Thr"){
+//     document.write("Work Day")
+// }else if(c === "Fri"){
+//     document.write("Work Day")
+// }
+
+
+// Task 5 //
+
+// Not Completed But it will be Complete //
+
+// var monthName = ["January", "February", "March", "april", "May", "june", "July", "aug", "sep", "oct", "november", "december"]
+// var now  = new Date();
+// var theMonth = now.getMonth();
+// var theDay = now.getDay();
+// var b = theDay.toString();
+// var c = theMonth.toString();
+// var nameOfMonth = monthName[theMonth] +  " " + [theDay];
+// console.log(nameOfMonth);
+
+// Task 6 //
+
+// var todayDate = new Date();
+// var Todaymilli = todayDate.getTime();
+// var todayMin = todayDate.getMinutes();
+// var diff = Todaymilli - todayMin;
+// var diffMin = diff/(1000*60*60);
+// var accurateMin = Math.floor(diffMin);
+
+// document.write("Current Date: " + todayDate);
+// document.write("<br> Elapsed milliseconds since midnight, Jan 1, 1970: " + Todaymilli);
+// document.write("<br> Elapsed minutes since midnight, Jan 1, 1970: " + accurateMin)
+
+// Task  7 //
+
+
+// var a = new Date();
+// var b = a.toString();
+// var c = b.slice(16,19);
+// if (c === "12:"){
+//     document.write("It's PM")
+// }else if(c === "13:"){
+//     document.write("It's PM")
+// }else if(c === "14:"){
+//     document.write("It's PM")
+// }else if(c === "15:"){
+//     document.write("It's PM")
+// }else if(c === "16:"){
+//     document.write("It's PM")
+// }else if(c === "17:"){
+//     document.write("It's PM")
+// }else if(c === "18:"){
+//     document.write("It's PM")
+// }else if(c === "19:"){
+//     document.write("It's PM")
+// }else if(c === "20:"){
+//     document.write("It's PM")
+// }else if(c === "21:"){
+//     document.write("It's PM")
+// }else if(c === "22:"){
+//     document.write("It's PM")
+// }else if(c === "23:"){
+//     document.write("It's PM")
+// }else if(c === "24:"){
+//     document.write("It's AM")
+// }else if(c === "01:"){
+//     document.write("It's AM")
+// }else if(c === "02:"){
+//     document.write("It's AM")
+// }else if(c === "03:"){
+//     document.write("It's AM")
+// }else if(c === "04:"){
+//     document.write("It's AM")
+// }else if(c === "05:"){
+//     document.write("It's AM")
+// }else if(c === "06:"){
+//     document.write("It's AM")
+// }else if(c === "07:"){
+//     document.write("It's AM")
+// }else if(c === "08:"){
+//     document.write("It's AM")
+// }else if(c === "09:"){
+//     document.write("It's AM")
+// }else if(c === "10:"){
+//     document.write("It's AM")
+// }else if(c === "11:"){
+//     document.write("It's AM")
+// }
+
+// Task 8 //
+
